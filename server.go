5a3e527547de8ae30f391988fb406a1f523d1ef2
Alexandre Figueiredo <alexandrefigueiredo@Alexandres-MacBook-Air.local>
2016-11-22 21:50:42 +0200

    moved away from the classic negroni.

--- server.go
+++ server.go
@@ -3,21 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/urfave/negroni"
 	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", homeHandler)
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", homeHandler)
 
-	n := negroni.Classic() // includes default middlewares
-	n.UseHandler(router)
+	n := negroni.New()
+	recovery := negroni.NewRecovery()
+	recovery.ErrorHandlerFunc = handleRecovery
+	n.Use(recovery)
+	n.Use(negroni.NewStatic(http.Dir("/public")))
+	n.UseHandler(mux)
 
-	n.Run(":8080")
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        n,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "welcome to the home page again")
+}
+
+func handleRecovery(error interface{}) {
+	// log error and try not to crash everything...
 }
